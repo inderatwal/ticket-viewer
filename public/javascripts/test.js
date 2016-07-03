26f3ee7d9c41ccc67797cf9f86cf0a5ee6062bba
inderatwal <inderpreet.atwal95@hotmail.com>
2016-07-03 12:22:56 +1000

    list page UI completely finished, details page being completed now, error handling is complete

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,12 +1,19 @@
 
 
+/*
+* this code is used to send a parameter in the url via the search  depending on which *   ticket is clicked 
+* the id is then recieved by the server, and is returned to the next details page
+*
+*/
 var id;
 $(document).on('click', '.btn', function() {
         var temp=this.id;
         id=parseInt(temp);
-	var parameters={ search : temp};
+        //pass clicked id as a paramater
+	var parameters={ search : temp}
+        //pass paramaters (id) into URL
 	$.get('/',parameters,function(data){
-		alert('this somehow worked');
+		// change windows to the new details page
 		window.location.href = '/details';
 	});
 	
@@ -14,38 +21,90 @@ $(document).on('click', '.btn', function() {
 		
 });
 
-$("#p1").click( function(){
-  document.getElementById("page1").style.display='block';
-  document.getElementById("page2").style.display='none';
-  document.getElementById("page3").style.display='none';
-  document.getElementById("page4").style.display='none';
- $('body').scrollTop(0);
-  
+
+/*
+* each link is given its own onclick event, and it displays a different hidden div
+* according to the button pressed, while hiding all other divs. 
+* 
+* this is the code used to navigate through each 25 tickets
+*
+*/
+
+
+$(document).on('click', '.p1', function() {
+	//displaying page 1, while hidding the rest
+	document.getElementById("page1").style.display='block';
+	document.getElementById("page2").style.display='none';
+	document.getElementById("page3").style.display='none';
+	document.getElementById("page4").style.display='none';
+	// send to top after clicking and displaying next page.
+	$('body').scrollTop(0);
+	
+		
 });
-$("#p2").click( function(){
-  document.getElementById("page1").style.display='none';
-  document.getElementById("page2").style.display='block';
-  document.getElementById("page3").style.display='none';
-  document.getElementById("page4").style.display='none';
-  $('body').scrollTop(0);
-  
-});
-$("#p3").click( function(){
-  document.getElementById("page1").style.display='none';
-  document.getElementById("page2").style.display='none';
-  document.getElementById("page3").style.display='block';
-  document.getElementById("page4").style.display='none';
-  $('body').scrollTop(0);
-});
-$("#p4").click( function(){
-  document.getElementById("page1").style.display='none';
-  document.getElementById("page2").style.display='none';
-  document.getElementById("page3").style.display='none';
-  document.getElementById("page4").style.display='block';
-  $('body').scrollTop(0);
+
+$(document).on('click', '.p2', function() {
+	//displaying page 2, while hidding the rest
+	document.getElementById("page1").style.display='none';
+	document.getElementById("page2").style.display='block';
+	document.getElementById("page3").style.display='none';
+	document.getElementById("page4").style.display='none';
+	// send to top after clicking and displaying next page.
+	$('body').scrollTop(0);
+	
+		
 });
 
 
+$(document).on('click', '.p3', function() {
+ 	//displaying page 3, while hidding the rest
+ 	document.getElementById("page1").style.display='none';
+	document.getElementById("page2").style.display='none';
+	document.getElementById("page3").style.display='block';
+	document.getElementById("page4").style.display='none';
+	// send to top after clicking and displaying next page.
+	$('body').scrollTop(0);
+	
+		
+});
 
+$(document).on('click', '.p4', function() {
+	//displaying page 4, while hidding the rest
+	document.getElementById("page1").style.display='none';
+	document.getElementById("page2").style.display='none';
+	document.getElementById("page3").style.display='none';
+	document.getElementById("page4").style.display='block';
+	// send to top of page after clicking and displaying next page.
+	$('body').scrollTop(0);
+		
+});
+
+
+$(document).on('click', '.backbtn', function() {
+        window.location.href = '/';
+	});
+	
+
+
+
+
+
+
+
+
+// code to keep header table visible while scrolling, due to large number of tickets. 
+$(window).scroll(function(e){ 
+	var $el = $('#sticky'); 
+	var isPositionFixed = ($el.css('position') == 'fixed');
+	if ($(this).scrollTop() > 100 && !isPositionFixed){ 
+		/*setting ccs variables to display sticky header 			created*/
+		$('#sticky').css({'position': 'fixed', 'top': '0px','display':'block'}); 
+	}
+	if ($(this).scrollTop() < 100 && isPositionFixed)
+	{
+		/*setting ccs variables to hide sticky header 			when at top of page again */
+		$('#sticky').css({'position': 'static', 'top': '0px', 'display':'none'}); 
+	} 
+});
 
 
--- routes/server.js
+++ routes/server.js
@@ -7,8 +7,10 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var val;
 // new request
 var req = new XMLHttpRequest();
+
 // open request with zendesk account name
 req.open("GET", "https://gabruapparel.zendesk.com/api/v2/tickets.json", false);
+
 //basic authorisation with username and password
 req.setRequestHeader("Authorization", "Basic " + new Buffer('inderpreet.atwal95@hotmail.com:Karan1998').toString('base64'));
 req.send();	
@@ -16,28 +18,44 @@ req.send();
 //debugging
 console.log(req.status);
 console.log(req.statusText);
+var status=req.status
+var tickData=null;
 
 // stored requested data
-var tickData = JSON.parse(req.responseText);	
-
-// debuggings, displaying ticket subject in console
-for (i = 0; i < tickData.tickets.length; i++) { 
-    console.log(tickData.tickets[i].subject);
+if(status!=0 && status!=404 && status!=401){
+	tickData = JSON.parse(req.responseText);
 }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Zendesk Ticket Viewer', data: tickData});
+
+/*website loaded if no errors*/
+if(status!=0 && status!=404 && status!=401 ){
+	res.render('index', { title: 'Zendesk Ticket Viewer', data: tickData});
+        //recieveing the URL search query, when ticket is selected
+	val= req.query.search;
+        console.log("connection made");
+	console.log(val)
+}
+/*error page displayed if no internet connection*/
+else if(status==0 || status==404  ){
+	res.render('error', { title: 'Error 404: No connection could be established with the API \n Please check your Internet Connection and try again'})
+	console.log("Error 404, no Connection could be made");
+}
+/*error page displayed if authentication failed*/
+else if(status==401 ){
+	res.render('error', { title: 'Error 401: Authentication Failed, Please check username & Password'})
+	console.log("Error 401, authentication Failed");
+}
   
-  console.log("item clicked: "+val);
   
 });
 
 
 /* GET details page */
 router.get('/details', function(req, res, next) {
- val= req.query.search;
- res.render('details', { title:'Ticket Details for ticket id:  '+val, dData: tickData, tId: val});
+ 
+res.render('details', { title:'Ticket Details for ticket id:  '+val, dData: tickData, tId: val});
  
  
 });
